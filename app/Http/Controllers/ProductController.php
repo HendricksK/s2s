7079bda4a58f695cd5c5c77b955dca1143e64b29
HendricksK <kurvinhendricks@gmail.com>
2021-06-07 00:11:03 +0200

    added composer auto loading of controllers and interface

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-// use App\Http\Controllers\ProductControllerMeta;
+use App\Http\Controllers\ProductControllerMeta;
 use App\Http\Interfaces\Crud;
 
 class ProductController implements Crud
